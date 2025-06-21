29f8d93a4446b04fd66c9b632673a773b38d8794
exo <setane122@gmail.com>
2025-06-21 02:32:36 +0100

    get user

--- controllers/User.controller.ts
+++ controllers/User.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import { PrismaClient } from "../generated/prisma";
 import { user } from "../generated/prisma";
 import bcrypt from "bcrypt";
+import { error } from "console";
 const client = new PrismaClient();
 // const emailChrck = (email: string) => {
 //   const path = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
@@ -60,6 +61,29 @@ const UserController = {
       res.status(400).json({
         error: error,
       });
+    }
+  },
+  getUserProfile: async (req: Request, res: Response) => {
+    try {
+      const { id } = req.params;
+      if (!id) res.status(400).json({ error: "provide id" });
+      const user = await client.user.findUnique({
+        where: {
+          id,
+        },
+      });
+      if (!user) {
+        res.status(400).json({
+          error: "user not found",
+        });
+      } else {
+        res.status(200).json({
+          msg: "get user",
+          data: user,
+        });
+      }
+    } catch (error) {
+      res.status(400).json({ error: error });
     }
   },
 };
--- routes/User.routes.ts
+++ routes/User.routes.ts
@@ -2,4 +2,6 @@ import { Router } from "express";
 import UserController from "../controllers/User.controller";
 const UserEndPoints = Router();
 UserEndPoints.post("/signup", UserController.creatUser);
+UserEndPoints.get("/profile/:id", UserController.getUserProfile);
+UserEndPoints.put("/profile/:id", UserController.updateUser);
 export default UserEndPoints;
