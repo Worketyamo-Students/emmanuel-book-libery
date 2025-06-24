ddf08cb9ffb1b4e3cf426c6edf99a0c34694b497
exo <setane122@gmail.com>
2025-06-24 11:58:30 +0100

    creat books routes and controller

---
+++ controllers/Book.controller.ts
@@ -0,0 +1,6 @@
+import { book } from "../generated/prisma";
+import { PrismaClient } from "../generated/prisma";
+
+const book = new PrismaClient();
+const bookController = {};
+export default bookController;
