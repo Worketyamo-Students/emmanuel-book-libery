969f123ecf4acfc0215dbe309071308eca663009
exo <setane122@gmail.com>
2025-06-24 12:46:00 +0100

    created user

--- controllers/Book.controller.ts
+++ controllers/Book.controller.ts
@@ -2,15 +2,36 @@ import { Request, Response } from "express";
 import { book } from "../generated/prisma";
 import { PrismaClient } from "../generated/prisma";
 
-const book = new PrismaClient();
+const client = new PrismaClient();
 const bookController = {
   getAllBook: async (req: Request, res: Response) => {
     try {
       req;
-      const books = await book.book.findMany();
+      const books = await client.book.findMany();
       res.status(200).json({ msg: "all book", data: books });
     } catch (error) {
       res.status(500).json({ msg: "intrernal error " });
+    }
+  },
+  createBook: async (req: Request, res: Response) => {
+    try {
+      const { title, description, author, publicationDate, ISBN }: book =
+        req.body;
+      if (!title || !description || !author || !publicationDate || !ISBN) {
+        res.status(400).json({ msg: "provide all the fields" });
+      }
+      const books = await client.book.create({
+        data: {
+          title,
+          description,
+          author,
+          publicationDate,
+          ISBN,
+        },
+      });
+      res.status(201).json({ msg: "book created", data: books });
+    } catch (error) {
+      res.status(500).json({ msg: "internal error " });
     }
   },
 };
